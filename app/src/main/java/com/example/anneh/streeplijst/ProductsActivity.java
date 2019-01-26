b2ef97a3130c28c954a1659bc46c2d9870d5721b
annehs <annehoogerduijn@gmail.com>
2019-01-26 17:17:39 +0100

    Navigation Drawer ProductsAcitivity

--- app/src/main/java/com/example/anneh/streeplijst/ProductsActivity.java
+++ app/src/main/java/com/example/anneh/streeplijst/ProductsActivity.java
@@ -2,6 +2,10 @@ package com.example.anneh.streeplijst;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +21,10 @@ import java.util.ArrayList;
 
 public class ProductsActivity extends AppCompatActivity {
 
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+
     private StreepDatabase db;
     Cursor productsCursor;
     private ProductAdapter adapter;
@@ -25,6 +33,18 @@ public class ProductsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+
+        // Set navigation drawer.
+        // https://medium.com/quick-code/android-navigation-drawer-e80f7fc2594f
+        drawer = (DrawerLayout)findViewById(R.id.activity_products);
+        toggle = new ActionBarDrawerToggle(this, drawer, R.string.Open, R.string.Close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Set listener for Navigation Drawer.
+        navigationView = (NavigationView)findViewById(R.id.nv);
+        navigationView.setNavigationItemSelectedListener(new NavigationViewClickListener());
 
         // Get cursor for products table.
         db = StreepDatabase.getInstance(getApplicationContext());
@@ -40,49 +60,52 @@ public class ProductsActivity extends AppCompatActivity {
         productGrid.setOnItemLongClickListener(new ProductsActivity.GridViewLongClickListener());
     }
 
-    // TODO: Sorteer producten op meest gestreept.
 
-    // TODO: zorgen dat dit niet elke activiteit opnieuw moet
-    // https://www.youtube.com/watch?v=o4WeEitmF9E
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    private class NavigationViewClickListener implements NavigationView.OnNavigationItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-        // Inflate the menu; this adds itemts to the action bar if it is present
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+            // Handle action bar item clicka: go to corresponding activity.
+            int id = item.getItemId();
+
+            if (id == R.id.overview) {
+                Intent intent = new Intent(ProductsActivity.this, OverviewActivity.class);
+                startActivity(intent);
+            }
+            else if (id == R.id.addProduct) {
+                Intent intent = new Intent(ProductsActivity.this, NewProductActivity.class);
+                startActivity(intent);
+            }
+            else if (id == R.id.addUser) {
+                Intent intent = new Intent(ProductsActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+            else if (id == R.id.export) {
+                Intent intent = new Intent(ProductsActivity.this, ExportActivity.class);
+                startActivity(intent);
+            }
+            else if (id == R.id.pin) {
+                Intent intent = new Intent(ProductsActivity.this, PinActivity.class);
+                startActivity(intent);
+            }
+
+            return true;
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        // Handle action bar item clicka: go to corresponding activity.
-        int id = item.getItemId();
-
-        if (id == R.id.overview) {
-            Intent intent = new Intent(ProductsActivity.this, OverviewActivity.class);
-            startActivity(intent);
-        }
-        else if (id == R.id.addProduct) {
-            Intent intent = new Intent(ProductsActivity.this, NewProductActivity.class);
-            startActivity(intent);
-        }
-        else if (id == R.id.addUser) {
-            Intent intent = new Intent(ProductsActivity.this, RegisterActivity.class);
-            startActivity(intent);
-        }
-        else if (id == R.id.export) {
-            Intent intent = new Intent(ProductsActivity.this, ExportActivity.class);
-            startActivity(intent);
-        }
-        else if (id == R.id.pin) {
-            Intent intent = new Intent(ProductsActivity.this, PinActivity.class);
-            startActivity(intent);
+        if(toggle.onOptionsItemSelected(item)) {
+            return true;
         }
 
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 
 
+
+    // TODO: Sorteer producten op meest gestreept.
     // Go to UsersActivity when product is clicked
     private class GridViewClickListener implements AdapterView.OnItemClickListener {
         @Override
